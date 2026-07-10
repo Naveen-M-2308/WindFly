e6781783de454074eca14ca2d5222052af2067e5
Naveen M <naveenamar23@gmail.com>
2026-07-10 20:19:06 +0530

    save3

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ResponsiveContainer from './components/ResponsiveContainer';
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <ResponsiveContainer>
-   <BrowserRouter>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-</ResponsiveContainer>
   </StrictMode>,
 );
