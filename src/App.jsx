a12fa9737e4be1247e8d1cb1d9a010987249796a
Naveen M <naveenamar23@gmail.com>
2026-07-30 18:13:35 +0530

    save301

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,23 @@ import About from "./pages/About";
 import Explore from "./pages/Explore";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
-import Book from './components/Book'
+import Cart from './components/Cart'
 import { Routes, Route } from "react-router-dom";
 
 
 function App() {
-  // const [bookItems, setBookitems] = useState([]);
+  const [bookItems, setBookitems] = useState([]);
 
   return (
     <>
-    
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/explore" element={<Explore />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        {/* <Route path="/cart" element={<Book myBook={bookItems}/>}></Route> */}
-
+        <Route path="/cart" element={<Cart myBook={bookItems}/>}></Route>
       </Routes>
-
    </>
   );
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart({mybook}) {
+  return (
+    <>
+    <div></div>
+    </>
+  )
+}
+
+export default Cart
